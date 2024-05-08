1d997aa3ecec40e0d82a58755c1424b56c1d162d
dihasdinsara <chaminda.email@gmail.com>
2024-05-08 13:50:38 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-//import ProductList from "@/components/ProductList"
+import ProductList from "@/components/ProductList"
 import Navbar from "@/components/Navbar"
 import Getstart from "@/components/Getstart";
 import Footer from "@/components/Footer";
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       
-      {/*<ProductList/>*/}
+      <ProductList/>
       <Getstart/>
       
       
